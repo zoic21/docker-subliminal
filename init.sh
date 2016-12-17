d94e16e6e324ed8c6868a14dfa57371fc11423ab
Loïc <loic@jeedom.com>
2016-12-17 17:02:44 +0100

    fix cron

--- init.sh
+++ init.sh
@@ -16,7 +16,7 @@ if [ -z ${WEEK} ]; then
 	WEEK=2
 fi
 
-(echo "* * * * * su --shell=/bin/bash - www-data -c 'python /root/subliminal.py -l ${LANG} -w ${WEEK}' >> /dev/null"; crontab -l | grep -v "subliminal" | crontab -
+(echo "00 00 * * * su --shell=/bin/bash - www-data -c 'python /root/subliminal.py -l ${LANG} -w ${WEEK}' >> /dev/null"; crontab -l | grep -v "subliminal" | crontab -
 
 /usr/bin/supervisord
 
